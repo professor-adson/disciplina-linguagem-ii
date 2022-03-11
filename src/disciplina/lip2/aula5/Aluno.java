f6fe8da5f6802f2f7db2040ef34eacb670e79da5
adson <adsonrocha@gmail.com>
2022-03-11 16:05:16 -0300

    .

---
+++ src/disciplina/lip2/aula5/Aluno.java
@@ -0,0 +1,20 @@
+package disciplina.lip2.aula5;
+
+import java.time.LocalDate;
+
+public class Aluno {
+    String nome;
+    int idade;
+    LocalDate dataMatricula;
+
+    public Aluno(String nome, int idade, LocalDate dataMatricula) {
+        this.nome = nome;
+        this.idade = idade;
+        this.dataMatricula = dataMatricula;
+    }
+
+    public void eMaior() {
+        String situacao = this.idade >= 18 ? "maior" : "menor";
+        System.out.println(situacao);
+    }
+}
