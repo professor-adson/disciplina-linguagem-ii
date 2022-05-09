b26c532795366b83d49799a0a84d0837bd282c9b
adson <adsonrocha@gmail.com>
2022-05-09 10:22:12 -0300

    .

--- src/aula1/Teste.java
+++ src/aula1/Teste.java
@@ -11,11 +11,10 @@ class Pato {
 
     public static void main(String[] args) {
         ArrayList<Pato> patos = new ArrayList<>();
-        Pato donald = new Pato(); patos.add(donald);
-        Pato pato = new Pato("Donald"); patos.add(donald);
+//        Pato donald = new Pato(); patos.add(donald);
+//        Pato pato = new Pato("Donald"); patos.add(donald);
         patos.add(new Pato("Donald"));
-        patos.add(new Donald());
-
+//        patos.add(new Donald());
     }
 }
 
